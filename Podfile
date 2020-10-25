749d8cefb4f759950e0c03c88d9c230a45ac000b
Stefanie Dao <ctdao@ucsd.edu>
2020-10-25 11:05:52 -0700

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'codepath_hw1' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for codepath_hw1
+  pod "AlamofireImage"
+
+  target 'codepath_hw1Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'codepath_hw1UITests' do
+    # Pods for testing
+  end
+
+end
---
+++ codepath_hw1/MovieDetailViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MovieDetailViewController.swift
+//  Flix
+//
+//  Created by Stephanie Dao on 10/24/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class MovieDetailViewController: UIViewController {
+    
+    var movie: [String: Any]!
+
+    @IBOutlet weak var backdrop: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var sypnosisLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        sypnosisLabel.text = movie["overview"] as? String
+        sypnosisLabel.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        posterView.af.setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdrop.af.setImage(withURL: backdropUrl!)
+        
+        
+
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
